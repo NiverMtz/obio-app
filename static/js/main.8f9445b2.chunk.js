8fee0f967be0025d4dd1bb66fb2a5fe026aa5c0f
NiverMtz <nivermartinezmed@gmail.com>
2021-07-05 17:22:16 -0500

    Updates

---
+++ static/js/main.8f9445b2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpobio-app"]=this["webpackJsonpobio-app"]||[]).push([[0],{9:function(e,t,o){"use strict";o.r(t);var a=o(1),n=o.n(a),p=o(3),s=o.n(p),c=o(0);var i=function(){return Object(c.jsx)("div",{className:"App",children:"Hola, aqu\xed comienza el prototipo y ajustes de esta app"})};s.a.render(Object(c.jsx)(n.a.StrictMode,{children:Object(c.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.8f9445b2.chunk.js.map
