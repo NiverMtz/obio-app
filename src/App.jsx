ce0544f308f40ef9c018a9b624f1732ba64b4e96
NiverMtz <nivermartinezmed@gmail.com>
2021-07-07 06:18:28 -0500

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Inicio.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
   return (
